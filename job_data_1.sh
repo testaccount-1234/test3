0d2af35994d95a5f5b932a0304dda93e6c98b462
Janina Schuette <schuette@bois.math.tu-berlin.de>
2020-09-04 15:08:46 +0200

    new old data

---
+++ job_data_1.sh
@@ -0,0 +1,1 @@
+python gen_data.py "{'measurements': 72, 'order': '3','leg_dimension': '4','file_name': 'legdim=4_meas=72', 'batchsize': '864' ,'minimal_l2loss': '0.00000001', 'minimal_step_size': '0.00000001'}"
