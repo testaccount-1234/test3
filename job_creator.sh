44a30c692fb6d019fe7751c615056b92b483db3c
testaccount-1234 <70666157+testaccount-1234@users.noreply.github.com>
2020-09-04 10:49:58 +0200

    Add files via upload

--- job_creator.sh
+++ job_creator.sh
@@ -4,7 +4,7 @@ dmax=50
 dskip=5
 order_of_tensor=3
 
-echo 'ok1'
+
 for ((legdim=dmin; legdim<=dmax; legdim+=dskip)); do
     batchsize=$((legdim**2))
     H=1
@@ -15,7 +15,8 @@ for ((legdim=dmin; legdim<=dmax; legdim+=dskip)); do
     echo $mmax
     echo $mskip
     for ((meas=mmin; meas<=mmax; meas+=mskip)); do
-    	echo 'ok3'
+    	echo $legdim
+        echo $meas
         echo "python recover_theta.py \"{'measurements': $meas, 'order': '$order_of_tensor','leg_dimension': '$legdim','file_name': 'legdim=${legdim}_meas=${meas}', 'batchsize': ${batchsize} ,'minimal_l2loss': '0.00000001', 'minimal_step_size': '0.00000001'}\"" > job_rec_$counter.sh
         ((counter++))
     done
